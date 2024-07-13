751ddb4338c3c72045951678e750da6159774a79
jessicaamarante <jessicaamarante1@gmail.com>
2024-07-13 08:59:26 -0300

    add create trip with prisma

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from 'fastify-type-provider-zod';
 import { z } from 'zod'
+import { prisma } from "../lib/prisma";
 
 export async function createTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post('/trips', {
@@ -14,10 +15,16 @@ export async function createTrip(app: FastifyInstance) {
 	}, async (request) => {
     const { destination, starts_at, ends_at } = request.body
 
+		const trip = await prisma.trip.create({
+			data: {
+				destination,
+				starts_at,
+				ends_at
+			}
+		})
+
 		return {
-			destination,
-			starts_at,
-			ends_at
+			tripId: trip.id
 		}
   })
 }
