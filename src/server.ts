c9f8c089f3a22945d8d8d4132d05d33236c192aa
jessicaamarante <jessicaamarante1@gmail.com>
2024-07-14 19:09:27 -0300

    add fastify/cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
-import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 import fastify from "fastify";
+import cors from '@fastify/cors'
+import { 
+  serializerCompiler, 
+  validatorCompiler 
+} from 'fastify-type-provider-zod';
 import { createTrip } from "./routes/create-trip";
 import { confirmTrip } from './routes/confirm-trip';
 
 const app = fastify()
+
+// cors eh usado para definir qual endereço frontend pode acessar o endpoint do app se deixar origin como true ou * - podera ser acessado de qualquer frontend
+app.register(cors, {
+  origin: '*'
+})
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
