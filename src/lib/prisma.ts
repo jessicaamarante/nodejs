d7b9350ae141b832be418b23d32c17e54932f9e7
jessicaamarante <jessicaamarante1@gmail.com>
2024-07-14 19:33:31 -0300

    test

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client"
 
-// mostra log quando faz uma query pro bd
+// mostra log quando faz uma query pro banco de dados
 export const prisma = new PrismaClient({
   log: ['query'],
 })
