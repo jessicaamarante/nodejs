4b318564b1f125aaaea489516b4d42bf6e3c27b8
jessicaamarante <jessicaamarante1@gmail.com>
2024-07-31 08:23:55 -0300

    create validations for when user click in link to confirm trip

--- src/routes/confirm-trip.ts
+++ src/routes/confirm-trip.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from "fastify"
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod' // use to schema validate
+import { prisma } from "../lib/prisma"
+import { dayjs } from "../lib/dayjs"
+import { getMailClient } from "../lib/mail"
 
 export async function confirmTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get('/trips/:tripId/confirm', {
@@ -9,7 +12,38 @@ export async function confirmTrip(app: FastifyInstance) {
 				tripId: z.string().uuid(),
 			})
 		},
-	}, async (request) => {
-    return { tripId: request.params.tripId }
-  })
+	}, async (request, reply) => {
+      const { tripId } = request.params // recebo por parametro o trip id
+
+      const trip = await prisma.trip.findUnique({ // consulta se existe alguma viagem para esse tripId
+        where: {
+          id: tripId,
+        }
+      })
+
+      if (!trip) {
+        throw new Error('Trip not found.')
+      }
+      // quando usuario ja confirmou a viagem fazemos um redirecionamento quando ele clicar no link de confirmacao, para fazer esse redirecionamento como to usando o fastfy 
+      // eh preciso pegar o segundo parametro da funcao  async (request, reply) que eh o reply
+      if (trip.is_confirmed) {
+        return reply.redirect(`http://localhost:3000/trips/${tripId}`)
+      }
+      //se a viagem ainda n foi confirmada...
+      await prisma.trip.update({
+        where: { id: tripId },
+        data: { is_confirmed: true },
+      })
+
+      const formattedStartDate = dayjs(trip.starts_at).format('LL')
+
+		  const formattedEndDate = dayjs(trip.ends_at).format('LL')
+
+		  const confirmationLink = `http://localhost:3333/trips/${trip.id}/confirm/ID_DO_PARTICIPANTE`
+
+		  const mail = await getMailClient()
+
+      return { tripId: request.params.tripId }
+    }
+  )
 }
