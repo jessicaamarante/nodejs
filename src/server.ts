f2a992dd34cc4c4e8162632d9f4fe9093d17e024
jessicaamarante <jessicaamarante1@gmail.com>
2024-08-04 17:26:52 -0300

    add confirm-participant route in server

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,9 @@ import {
   validatorCompiler 
 } from 'fastify-type-provider-zod';
 import { createTrip } from "./routes/create-trip";
-import { confirmTrip } from './routes/confirm-trip';
+import { confirmTrip } from "./routes/confirm-trip";
+import { confirmParticipant } from "./routes/confirm-participant";
+
 
 const app = fastify()
 
@@ -19,6 +21,7 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip)
 app.register(confirmTrip)
+app.register(confirmParticipant)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running!')
